2ca9358d23290faac7c160c7b54d5eeeb64fca8c
Mateusz Pawlikowski <mateuszpawlikowski98@gmail.com>
2023-01-18 19:20:36 +0100

    FpCondition implementation

---
+++ FunctionalCSharp/FpCondition/ICondition.cs
@@ -0,0 +1,18 @@
+﻿namespace FunctionalCSharp;
+
+public interface ICondition<T>
+{
+    public T? Evaluate();
+}
+
+public interface IConditionStep<T> : ICondition<T>
+{
+
+    public IConditionStep<T> Elif(bool condition, T then);
+    public IConditionStep<T> Elif(Func<bool> condition, T then);
+    public IConditionStep<T> Elif(bool condition, Func<T> then);
+    public IConditionStep<T> Elif(Func<bool> condition, Func<T> then);
+    public ICondition<T> Else(T value);
+    public ICondition<T> Else(Func<T> expression);
+}
+
---
+++ FunctionalCSharp/FpCondition/IfBase.cs
@@ -0,0 +1,36 @@
+﻿namespace FunctionalCSharp;
+
+internal abstract class IfBase<T> : IConditionStep<T>
+{
+    protected IConditionStep<T>? @else;
+
+    public IConditionStep<T> Elif(bool condition, T then)
+    {
+        @else = new IfValue<T>(condition, then);
+        return @else;
+    }
+
+    public IConditionStep<T> Elif(Func<bool> condition, T then)
+    {
+        @else = new IfValue<T>(condition, then);
+        return @else;
+    }
+
+    public IConditionStep<T> Elif(bool condition, Func<T> then)
+    {
+        @else = new IfExpression<T>(condition, then);
+        return @else;
+    }
+
+    public IConditionStep<T> Elif(Func<bool> condition, Func<T> then)
+    {
+        @else = new IfExpression<T>(condition, then);
+        return @else;
+    }
+
+    public abstract ICondition<T> Else(T value);
+
+    public abstract ICondition<T> Else(Func<T> expression);
+
+    public abstract T? Evaluate();
+}
