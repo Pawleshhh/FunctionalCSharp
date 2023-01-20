9bfabe393d30b2fc29bbfdbce7b2d225bc6bee79
Mateusz Pawlikowski <mateuszpawlikowski98@gmail.com>
2023-01-20 19:22:45 +0100

    FpCondition Tests

--- FunctionalCSharp/FpCondition/IfExpression.cs
+++ FunctionalCSharp/FpCondition/IfExpression.cs
@@ -2,8 +2,6 @@
 
 internal class IfExpression<T> : IfBase<T>
 {
-    private bool? condition;
-    private Func<bool>? funcCondition;
     private Func<T>? then;
 
     internal IfExpression(bool condition, Func<T> then)
@@ -18,27 +16,15 @@ internal class IfExpression<T> : IfBase<T>
         this.then = then;
     }
 
-    public override ICondition<T> Else(T value)
-    {
-        then = value.AsFunc();
-        return this;
-    }
-
-    public override ICondition<T> Else(Func<T> expression)
-    {
-        then = expression;
-        return this;
-    }
-
-    public override T? Evaluate()
+    protected override T? EvaluateStep()
     {
         if (condition.HasValue)
         {
-            return condition.Value ? then!() : (@else is not null ? @else.Evaluate() : default);
+            return condition.Value ? then!() : (ElseStep is not null ? ElseStep.Evaluate() : default);
         }
         else
         {
-            return funcCondition!() ? then!() : (@else is not null ? @else.Evaluate() : default);
+            return funcCondition!() ? then!() : (ElseStep is not null ? ElseStep.Evaluate() : default);
         }
     }
 
