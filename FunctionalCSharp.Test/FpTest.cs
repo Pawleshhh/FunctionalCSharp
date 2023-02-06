3e7205f44849f992aed51350f7c5f84e60609bcd
Mateusz Pawlikowski <mateuszpawlikowski98@gmail.com>
2023-02-06 19:36:34 +0100

    Fp.AsFunc unit test

---
+++ FunctionalCSharp.Test/FpTest.cs
@@ -0,0 +1,20 @@
+﻿namespace FunctionalCSharp.Test;
+
+[TestFixture]
+internal class FpTest
+{
+
+    [Test]
+    public void AsFunc_PassValue_ResultOfFuncIsThePassedValue()
+    {
+        // ARRANGE
+        object value = new object();
+
+        // ACT
+        var result = Fp.AsFunc(value);
+
+        // ASSERT
+        Assert.That(result(), Is.SameAs(value));
+    }
+
+}
