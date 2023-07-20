371f731744d6dd2e2d4b7f0a01a26799e84b1620
Mateusz Pawlikowski <mateuszpawlikowski98@gmail.com>
2023-07-20 21:06:23 +0200

    Asynchronous FpUnit

--- FunctionalCSharp/Unit/FpUnit.cs
+++ FunctionalCSharp/Unit/FpUnit.cs
@@ -8,6 +8,8 @@ public static partial class Fp
     /// </summary>
     public static readonly Unit UnitValue = new();
 
+    #region Unit
+
     /// <summary>
     /// Invokes provided <paramref name="action"/> delegate and returns <see cref="FunctionalCSharp.Unit"/> value.
     /// </summary>
@@ -225,5 +227,229 @@ public static partial class Fp
         action(t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12, t13, t14, t15, t16);
         return UnitValue;
     }
+
+    #endregion
+
+    #region Async Unit
+
+    /// <summary>
+    /// Invokes provided <paramref name="action"/> delegate asynchronously and returns <see cref="FunctionalCSharp.Unit"/> value.
+    /// </summary>
+    /// <param name="action">Action to be invoked.</param>
+    /// <returns>Returns <see cref="FunctionalCSharp.Unit"/> value.</returns>
+    public static async Task<Unit> UnitAsync(this Action action)
+    {
+        await Task.Run(action);
+        return UnitValue;
+    }
+
+    /// <summary>
+    /// Invokes provided <paramref name="action"/> delegate asynchronously and returns <see cref="FunctionalCSharp.Unit"/> value.
+    /// </summary>
+    /// <param name="action">Action to be invoked.</param>
+    /// <returns>Returns <see cref="FunctionalCSharp.Unit"/> value.</returns>
+    public static async Task<Unit> UnitAsync<T>
+        (this Action<T> action, T t)
+    {
+        await Task.Run(() => action(t));
+        return UnitValue;
+    }
+
+    /// <summary>
+    /// Invokes provided <paramref name="action"/> delegate asynchronously and returns <see cref="FunctionalCSharp.Unit"/> value.
+    /// </summary>
+    /// <param name="action">Action to be invoked.</param>
+    /// <returns>Returns <see cref="FunctionalCSharp.Unit"/> value.</returns>
+    public static async Task<Unit> UnitAsync<T1, T2>
+        (this Action<T1, T2> action,
+        T1 t1, T2 t2)
+    {
+        await Task.Run(() => action(t1, t2));
+        return UnitValue;
+    }
+
+    /// <summary>
+    /// Invokes provided <paramref name="action"/> delegate asynchronously and returns <see cref="FunctionalCSharp.Unit"/> value.
+    /// </summary>
+    /// <param name="action">Action to be invoked.</param>
+    /// <returns>Returns <see cref="FunctionalCSharp.Unit"/> value.</returns>
+    public static async Task<Unit> UnitAsync<T1, T2, T3>
+        (this Action<T1, T2, T3> action,
+        T1 t1, T2 t2, T3 t3)
+    {
+        await Task.Run(() => action(t1, t2, t3));
+        return UnitValue;
+    }
+
+    /// <summary>
+    /// Invokes provided <paramref name="action"/> delegate asynchronously and returns <see cref="FunctionalCSharp.Unit"/> value.
+    /// </summary>
+    /// <param name="action">Action to be invoked.</param>
+    /// <returns>Returns <see cref="FunctionalCSharp.Unit"/> value.</returns>
+    public static async Task<Unit> UnitAsync<T1, T2, T3, T4>
+        (this Action<T1, T2, T3, T4> action,
+        T1 t1, T2 t2, T3 t3, T4 t4)
+    {
+        await Task.Run(() => action(t1, t2, t3, t4));
+        return UnitValue;
+    }
+
+    /// <summary>
+    /// Invokes provided <paramref name="action"/> delegate asynchronously and returns <see cref="FunctionalCSharp.Unit"/> value.
+    /// </summary>
+    /// <param name="action">Action to be invoked.</param>
+    /// <returns>Returns <see cref="FunctionalCSharp.Unit"/> value.</returns>
+    public static async Task<Unit> UnitAsync<T1, T2, T3, T4, T5>
+        (this Action<T1, T2, T3, T4, T5> action,
+        T1 t1, T2 t2, T3 t3, T4 t4, T5 t5)
+    {
+        await Task.Run(() => action(t1, t2, t3, t4, t5));
+        return UnitValue;
+    }
+
+    /// <summary>
+    /// Invokes provided <paramref name="action"/> delegate asynchronously and returns <see cref="FunctionalCSharp.Unit"/> value.
+    /// </summary>
+    /// <param name="action">Action to be invoked.</param>
+    /// <returns>Returns <see cref="FunctionalCSharp.Unit"/> value.</returns>
+    public static async Task<Unit> UnitAsync<T1, T2, T3, T4, T5, T6>
+        (this Action<T1, T2, T3, T4, T5, T6> action,
+        T1 t1, T2 t2, T3 t3, T4 t4, T5 t5, T6 t6)
+    {
+        await Task.Run(() => action(t1, t2, t3, t4, t5, t6));
+        return UnitValue;
+    }
+
+    /// <summary>
+    /// Invokes provided <paramref name="action"/> delegate asynchronously and returns <see cref="FunctionalCSharp.Unit"/> value.
+    /// </summary>
+    /// <param name="action">Action to be invoked.</param>
+    /// <returns>Returns <see cref="FunctionalCSharp.Unit"/> value.</returns>
+    public static async Task<Unit> UnitAsync<T1, T2, T3, T4, T5, T6, T7>
+        (this Action<T1, T2, T3, T4, T5, T6, T7> action,
+        T1 t1, T2 t2, T3 t3, T4 t4, T5 t5, T6 t6, T7 t7)
+    {
+        await Task.Run(() => action(t1, t2, t3, t4, t5, t6, t7));
+        return UnitValue;
+    }
+
+    /// <summary>
+    /// Invokes provided <paramref name="action"/> delegate asynchronously and returns <see cref="FunctionalCSharp.Unit"/> value.
+    /// </summary>
+    /// <param name="action">Action to be invoked.</param>
+    /// <returns>Returns <see cref="FunctionalCSharp.Unit"/> value.</returns>
+    public static async Task<Unit> UnitAsync<T1, T2, T3, T4, T5, T6, T7, T8>
+        (this Action<T1, T2, T3, T4, T5, T6, T7, T8> action,
+        T1 t1, T2 t2, T3 t3, T4 t4, T5 t5, T6 t6, T7 t7, T8 t8)
+    {
+        await Task.Run(() => action(t1, t2, t3, t4, t5, t6, t7, t8));
+        return UnitValue;
+    }
+
+    /// <summary>
+    /// Invokes provided <paramref name="action"/> delegate asynchronously and returns <see cref="FunctionalCSharp.Unit"/> value.
+    /// </summary>
+    /// <param name="action">Action to be invoked.</param>
+    /// <returns>Returns <see cref="FunctionalCSharp.Unit"/> value.</returns>
+    public static async Task<Unit> UnitAsync<T1, T2, T3, T4, T5, T6, T7, T8, T9>
+        (this Action<T1, T2, T3, T4, T5, T6, T7, T8, T9> action,
+        T1 t1, T2 t2, T3 t3, T4 t4, T5 t5, T6 t6, T7 t7, T8 t8, T9 t9)
+    {
+        await Task.Run(() => action(t1, t2, t3, t4, t5, t6, t7, t8, t9));
+        return UnitValue;
+    }
+
+    /// <summary>
+    /// Invokes provided <paramref name="action"/> delegate asynchronously and returns <see cref="FunctionalCSharp.Unit"/> value.
+    /// </summary>
+    /// <param name="action">Action to be invoked.</param>
+    /// <returns>Returns <see cref="FunctionalCSharp.Unit"/> value.</returns>
+    public static async Task<Unit> UnitAsync<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10>
+        (this Action<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10> action,
+        T1 t1, T2 t2, T3 t3, T4 t4, T5 t5, T6 t6, T7 t7, T8 t8, T9 t9, T10 t10)
+    {
+        await Task.Run(() => action(t1, t2, t3, t4, t5, t6, t7, t8, t9, t10));
+        return UnitValue;
+    }
+
+    /// <summary>
+    /// Invokes provided <paramref name="action"/> delegate asynchronously and returns <see cref="FunctionalCSharp.Unit"/> value.
+    /// </summary>
+    /// <param name="action">Action to be invoked.</param>
+    /// <returns>Returns <see cref="FunctionalCSharp.Unit"/> value.</returns>
+    public static async Task<Unit> UnitAsync<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11>
+        (this Action<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11> action,
+        T1 t1, T2 t2, T3 t3, T4 t4, T5 t5, T6 t6, T7 t7, T8 t8, T9 t9, T10 t10, T11 t11)
+    {
+        await Task.Run(() => action(t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11));
+        return UnitValue;
+    }
+
+    /// <summary>
+    /// Invokes provided <paramref name="action"/> delegate asynchronously and returns <see cref="FunctionalCSharp.Unit"/> value.
+    /// </summary>
+    /// <param name="action">Action to be invoked.</param>
+    /// <returns>Returns <see cref="FunctionalCSharp.Unit"/> value.</returns>
+    public static async Task<Unit> UnitAsync<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12>
+        (this Action<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12> action,
+        T1 t1, T2 t2, T3 t3, T4 t4, T5 t5, T6 t6, T7 t7, T8 t8, T9 t9, T10 t10, T11 t11, T12 t12)
+    {
+        await Task.Run(() => action(t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12));
+        return UnitValue;
+    }
+
+    /// <summary>
+    /// Invokes provided <paramref name="action"/> delegate asynchronously and returns <see cref="FunctionalCSharp.Unit"/> value.
+    /// </summary>
+    /// <param name="action">Action to be invoked.</param>
+    /// <returns>Returns <see cref="FunctionalCSharp.Unit"/> value.</returns>
+    public static async Task<Unit> UnitAsync<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13>
+        (this Action<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13> action,
+        T1 t1, T2 t2, T3 t3, T4 t4, T5 t5, T6 t6, T7 t7, T8 t8, T9 t9, T10 t10, T11 t11, T12 t12, T13 t13)
+    {
+        await Task.Run(() => action(t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12, t13));
+        return UnitValue;
+    }
+
+    /// <summary>
+    /// Invokes provided <paramref name="action"/> delegate asynchronously and returns <see cref="FunctionalCSharp.Unit"/> value.
+    /// </summary>
+    /// <param name="action">Action to be invoked.</param>
+    /// <returns>Returns <see cref="FunctionalCSharp.Unit"/> value.</returns>
+    public static async Task<Unit> UnitAsync<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14>
+        (this Action<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14> action,
+        T1 t1, T2 t2, T3 t3, T4 t4, T5 t5, T6 t6, T7 t7, T8 t8, T9 t9, T10 t10, T11 t11, T12 t12, T13 t13, T14 t14)
+    {
+        await Task.Run(() => action(t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12, t13, t14));
+        return UnitValue;
+    }
+
+    /// <summary>
+    /// Invokes provided <paramref name="action"/> delegate asynchronously and returns <see cref="FunctionalCSharp.Unit"/> value.
+    /// </summary>
+    /// <param name="action">Action to be invoked.</param>
+    /// <returns>Returns <see cref="FunctionalCSharp.Unit"/> value.</returns>
+    public static async Task<Unit> UnitAsync<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15>
+        (this Action<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15> action,
+        T1 t1, T2 t2, T3 t3, T4 t4, T5 t5, T6 t6, T7 t7, T8 t8, T9 t9, T10 t10, T11 t11, T12 t12, T13 t13, T14 t14, T15 t15)
+    {
+        await Task.Run(() => action(t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12, t13, t14, t15));
+        return UnitValue;
+    }
+
+    /// <summary>
+    /// Invokes provided <paramref name="action"/> delegate asynchronously and returns <see cref="FunctionalCSharp.Unit"/> value.
+    /// </summary>
+    /// <param name="action">Action to be invoked.</param>
+    /// <returns>Returns <see cref="FunctionalCSharp.Unit"/> value.</returns>
+    public static async Task<Unit> UnitAsync<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16>
+        (this Action<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16> action,
+        T1 t1, T2 t2, T3 t3, T4 t4, T5 t5, T6 t6, T7 t7, T8 t8, T9 t9, T10 t10, T11 t11, T12 t12, T13 t13, T14 t14, T15 t15, T16 t16)
+    {
+        await Task.Run(() => action(t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12, t13, t14, t15, t16));
+        return UnitValue;
+    }
+
+    #endregion
 
 }
--- FunctionalCSharp/Unit/Unit.cs
+++ FunctionalCSharp/Unit/Unit.cs
@@ -11,6 +11,8 @@ public class Unit
     /// </summary>
     public void Void() { }
 
+    #region Create
+
     /// <summary>
     /// Converts given <paramref name="action"/> to a func delegate.
     /// </summary>
@@ -215,5 +217,216 @@ public class Unit
             action(t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12, t13, t14, t15, t16);
             return Fp.UnitValue;
         };
+
+    #endregion
+
+    #region CreateAsync
+
+    /// <summary>
+    /// Converts given <paramref name="action"/> to an asynchronous delegate.
+    /// </summary>
+    /// <param name="action">Delegate to be converted.</param>
+    /// <returns>Returns converted <paramref name="action"/> to an asynchronous delegate.</returns>
+    public static Func<Task<Unit>> CreateAsync
+        (Action action)
+        => async () =>
+        {
+            await Task.Run(action);
+            return Fp.UnitValue;
+        };
+    /// <summary>
+    /// Converts given <paramref name="action"/> to an asynchronous delegate.
+    /// </summary>
+    /// <param name="action">Delegate to be converted.</param>
+    /// <returns>Returns converted <paramref name="action"/> to an asynchronous delegate.</returns>
+    public static Func<T, Task<Unit>> CreateAsync<T>
+        (Action<T> action)
+        => async t =>
+        {
+            await Task.Run(() => action(t));
+            return Fp.UnitValue;
+        };
+    /// <summary>
+    /// Converts given <paramref name="action"/> to an asynchronous delegate.
+    /// </summary>
+    /// <param name="action">Delegate to be converted.</param>
+    /// <returns>Returns converted <paramref name="action"/> to an asynchronous delegate.</returns>
+    public static Func<T1, T2, Task<Unit>> CreateAsync<T1, T2>
+        (Action<T1, T2> action)
+        => async (t1, t2) =>
+        {
+            await Task.Run(() => action(t1, t2));
+            return Fp.UnitValue;
+        };
+    /// <summary>
+    /// Converts given <paramref name="action"/> to an asynchronous delegate.
+    /// </summary>
+    /// <param name="action">Delegate to be converted.</param>
+    /// <returns>Returns converted <paramref name="action"/> to an asynchronous delegate.</returns>
+    public static Func<T1, T2, T3, Task<Unit>> CreateAsync<T1, T2, T3>
+        (Action<T1, T2, T3> action)
+        => async (t1, t2, t3) =>
+        {
+            await Task.Run(() => action(t1, t2, t3));
+            return Fp.UnitValue;
+        };
+    /// <summary>
+    /// Converts given <paramref name="action"/> to an asynchronous delegate.
+    /// </summary>
+    /// <param name="action">Delegate to be converted.</param>
+    /// <returns>Returns converted <paramref name="action"/> to an asynchronous delegate.</returns>
+    public static Func<T1, T2, T3, T4, Task<Unit>> CreateAsync<T1, T2, T3, T4>
+        (Action<T1, T2, T3, T4> action)
+        => async (t1, t2, t3, t4) =>
+        {
+            await Task.Run(() => action(t1, t2, t3, t4));
+            return Fp.UnitValue;
+        };
+    /// <summary>
+    /// Converts given <paramref name="action"/> to an asynchronous delegate.
+    /// </summary>
+    /// <param name="action">Delegate to be converted.</param>
+    /// <returns>Returns converted <paramref name="action"/> to an asynchronous delegate.</returns>
+    public static Func<T1, T2, T3, T4, T5, Task<Unit>> CreateAsync<T1, T2, T3, T4, T5>
+        (Action<T1, T2, T3, T4, T5> action)
+        => async (t1, t2, t3, t4, t5) =>
+        {
+            await Task.Run(() => action(t1, t2, t3, t4, t5));
+            return Fp.UnitValue;
+        };
+    /// <summary>
+    /// Converts given <paramref name="action"/> to an asynchronous delegate.
+    /// </summary>
+    /// <param name="action">Delegate to be converted.</param>
+    /// <returns>Returns converted <paramref name="action"/> to an asynchronous delegate.</returns>
+    public static Func<T1, T2, T3, T4, T5, T6, Task<Unit>> CreateAsync<T1, T2, T3, T4, T5, T6>
+        (Action<T1, T2, T3, T4, T5, T6> action)
+        => async (t1, t2, t3, t4, t5, t6) =>
+        {
+            await Task.Run(() => action(t1, t2, t3, t4, t5, t6));
+            return Fp.UnitValue;
+        };
+    /// <summary>
+    /// Converts given <paramref name="action"/> to an asynchronous delegate.
+    /// </summary>
+    /// <param name="action">Delegate to be converted.</param>
+    /// <returns>Returns converted <paramref name="action"/> to an asynchronous delegate.</returns>
+    public static Func<T1, T2, T3, T4, T5, T6, T7, Task<Unit>> CreateAsync<T1, T2, T3, T4, T5, T6, T7>
+        (Action<T1, T2, T3, T4, T5, T6, T7> action)
+        => async (t1, t2, t3, t4, t5, t6, t7) =>
+        {
+            await Task.Run(() => action(t1, t2, t3, t4, t5, t6, t7));
+            return Fp.UnitValue;
+        };
+    /// <summary>
+    /// Converts given <paramref name="action"/> to an asynchronous delegate.
+    /// </summary>
+    /// <param name="action">Delegate to be converted.</param>
+    /// <returns>Returns converted <paramref name="action"/> to an asynchronous delegate.</returns>
+    public static Func<T1, T2, T3, T4, T5, T6, T7, T8, Task<Unit>> CreateAsync<T1, T2, T3, T4, T5, T6, T7, T8>
+        (Action<T1, T2, T3, T4, T5, T6, T7, T8> action)
+        => async (t1, t2, t3, t4, t5, t6, t7, t8) =>
+        {
+            await Task.Run(() => action(t1, t2, t3, t4, t5, t6, t7, t8));
+            return Fp.UnitValue;
+        };
+    /// <summary>
+    /// Converts given <paramref name="action"/> to an asynchronous delegate.
+    /// </summary>
+    /// <param name="action">Delegate to be converted.</param>
+    /// <returns>Returns converted <paramref name="action"/> to an asynchronous delegate.</returns>
+    public static Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, Task<Unit>> CreateAsync<T1, T2, T3, T4, T5, T6, T7, T8, T9>
+        (Action<T1, T2, T3, T4, T5, T6, T7, T8, T9> action)
+        => async (t1, t2, t3, t4, t5, t6, t7, t8, t9) =>
+        {
+            await Task.Run(() => action(t1, t2, t3, t4, t5, t6, t7, t8, t9));
+            return Fp.UnitValue;
+        };
+    /// <summary>
+    /// Converts given <paramref name="action"/> to an asynchronous delegate.
+    /// </summary>
+    /// <param name="action">Delegate to be converted.</param>
+    /// <returns>Returns converted <paramref name="action"/> to an asynchronous delegate.</returns>
+    public static Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, Task<Unit>> CreateAsync<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10>
+        (Action<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10> action)
+        => async (t1, t2, t3, t4, t5, t6, t7, t8, t9, t10) =>
+        {
+            await Task.Run(() => action(t1, t2, t3, t4, t5, t6, t7, t8, t9, t10));
+            return Fp.UnitValue;
+        };
+    /// <summary>
+    /// Converts given <paramref name="action"/> to an asynchronous delegate.
+    /// </summary>
+    /// <param name="action">Delegate to be converted.</param>
+    /// <returns>Returns converted <paramref name="action"/> to an asynchronous delegate.</returns>
+    public static Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, Task<Unit>> CreateAsync<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11>
+        (Action<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11> action)
+        => async (t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11) =>
+        {
+            await Task.Run(() => action(t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11));
+            return Fp.UnitValue;
+        };
+    /// <summary>
+    /// Converts given <paramref name="action"/> to an asynchronous delegate.
+    /// </summary>
+    /// <param name="action">Delegate to be converted.</param>
+    /// <returns>Returns converted <paramref name="action"/> to an asynchronous delegate.</returns>
+    public static Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, Task<Unit>> CreateAsync<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12>
+        (Action<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12> action)
+        => async (t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12) =>
+        {
+            await Task.Run(() => action(t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12));
+            return Fp.UnitValue;
+        };
+    /// <summary>
+    /// Converts given <paramref name="action"/> to an asynchronous delegate.
+    /// </summary>
+    /// <param name="action">Delegate to be converted.</param>
+    /// <returns>Returns converted <paramref name="action"/> to an asynchronous delegate.</returns>
+    public static Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, Task<Unit>> CreateAsync<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13>
+        (Action<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13> action)
+        => async (t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12, t13) =>
+        {
+            await Task.Run(() => action(t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12, t13));
+            return Fp.UnitValue;
+        };
+    /// <summary>
+    /// Converts given <paramref name="action"/> to an asynchronous delegate.
+    /// </summary>
+    /// <param name="action">Delegate to be converted.</param>
+    /// <returns>Returns converted <paramref name="action"/> to an asynchronous delegate.</returns>
+    public static Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, Task<Unit>> CreateAsync<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14>
+        (Action<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14> action)
+        => async (t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12, t13, t14) =>
+        {
+            await Task.Run(() => action(t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12, t13, t14));
+            return Fp.UnitValue;
+        };
+    /// <summary>
+    /// Converts given <paramref name="action"/> to an asynchronous delegate.
+    /// </summary>
+    /// <param name="action">Delegate to be converted.</param>
+    /// <returns>Returns converted <paramref name="action"/> to an asynchronous delegate.</returns>
+    public static Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, Task<Unit>> CreateAsync<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15>
+        (Action<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15> action)
+        => async (t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12, t13, t14, t15) =>
+        {
+            await Task.Run(() => action(t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12, t13, t14, t15));
+            return Fp.UnitValue;
+        };
+    /// <summary>
+    /// Converts given <paramref name="action"/> to an asynchronous delegate.
+    /// </summary>
+    /// <param name="action">Delegate to be converted.</param>
+    /// <returns>Returns converted <paramref name="action"/> to an asynchronous delegate.</returns>
+    public static Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, Task<Unit>> CreateAsync<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16>
+        (Action<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16> action)
+        => async (t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12, t13, t14, t15, t16) =>
+        {
+            await Task.Run(() => action(t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12, t13, t14, t15, t16));
+            return Fp.UnitValue;
+        };
+
+    #endregion
 
 }
